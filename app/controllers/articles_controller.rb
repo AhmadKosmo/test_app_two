7b2a2f88da2b3d27b4fd338c6b2ee1902f896972
Ahmad Moussa <ahmad.moussa@kosmopolead.com>
2019-02-28 16:50:21 +0100

    Restrict Actions from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ def create
   # debugger
   #render plain: params[:article].inspect
   @article = Article.new(article_params)
-  @article.user = User.first
+  @article.user = User.find(current_user.id)
   if @article.save
     flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -15,16 +16,16 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to alpha blog #{@user.username}"
       redirect_to articles_path
     else
-    render 'new'
+      render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
@@ -35,11 +36,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
